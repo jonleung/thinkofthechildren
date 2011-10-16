eff5d1edc9de286271e6e9986d4bcdb9505231d3
jonathan <naruto137@gmail.com>
2011-10-15 21:33:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinkOfTheChildren::Application.config.secret_token = 'faa6a7282655ec3b32322d6e65766289669a43a2f74db6fec4e69b0ddf8c3e7240fcf0bef25ffb342b9dd7cbbfca4d9967628f64251a4e3d8accda44227a8b59'
