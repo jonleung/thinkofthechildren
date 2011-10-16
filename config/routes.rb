6bca6f9d7f004dbd83546dc8a10b8f2118ce83c1
Ben Shyong <likeachild@gmail.com>
2011-10-15 21:58:17 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ThinkOfTheChildren::Application.routes.draw do
 
   get "home/index"
+  
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
